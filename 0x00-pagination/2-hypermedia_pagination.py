c23ff2950a1a84872bf74441ff70ca253f8b655a
enimaos <ing.devov@gmail.com>
2024-07-22 21:28:49 +0100

    tiit

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
-"""
-task 2
-"""
-
-import math
+""" module doc """
 import csv
-from typing import List
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """index range"""
-    return ((page - 1) * page_size, page * page_size)
+import math
+from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """func doc"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,38 +25,27 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get a page of data from the dataset.
-        """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        """Get page from the dataset"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = self.index_range(page, page_size)
+        return self.dataset()[start:end]
 
-        self.dataset()
-        start, end = index_range(page, page_size)
-        return self.__dataset[start:end]
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """function doc"""
+        startPage = (page - 1) * page_size
+        endPage = page * page_size
+        return (startPage, endPage)
 
-    def get_hyper(self, page: int, page_size: int) -> tuple:
-        """
-        task 2
-        """
-        prev_page = 0
-        next_page = 0
-        total_pages = Server.dataset(self)
-        if page - 1 > 0:
-            prev_page = page - 1
-        else:
-            prev_page = None
-        if (page + 1) < total_pages:
-            next_page = page + 1
-        else:
-            next_page = None
-
-        dict_ = {}
-        dict_["page_size"] = page_size,
-        dict_["page"] = page
-        dict_["data"] = Server.get_page(page, page_size)
-        dict_["next_page"]: next_page
-        dict_["prev_page"]: prev_page
-        dict_["total_pages"]: total_pages
-
-        return dict_
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """function doc"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages,
+        }
